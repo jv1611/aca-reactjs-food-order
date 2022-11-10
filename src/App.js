05c789fb4d48428b692500cb47417a2ddf76345c
Jochen Vervoort <jochenvervoort@gmail.com>
2022-11-10 15:26:42 +0100

    added cart modal Manu

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import Header from './components/Layout/Header';
-import Meals from './components/Meals/Meals';
-import CartProvider from './store/CartProvider';
+import Cart from "./components/Cart/Cart";
+import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
+import CartProvider from "./store/CartProvider";
 
 function App() {
   return (
     <CartProvider>
+      <Cart />
       <Header />
       <main>
         <Meals />
